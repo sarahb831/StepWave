9eb648de3975a2886cb24faef7c8bb2c10dd98c8
sarahb831 <smbeauchamp885@gmail.com>
2020-07-22 20:31:14 -0700

    remove test component

--- src/App.js
+++ src/App.js
@@ -3,28 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar.js';
 import Intro from './components/Intro.js';
-import TestComponent from './components/TestComponent.js';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      {/*<header className="App-header">
-        <img src={logo} className="App-logo" alt="StepWave logo" />
-        <TestComponent />
-        <p>
-          Welcome to StepWave.
-        </p>
-        <a
-          className="App-link"
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Come on in
-        </a>
-      </header>
-  */}
       <Intro />
     </div>
   );
