dca5be6d315d3e16ec480450cb4a8c6e5be9c39f
sarahb831 <smbeauchamp885@gmail.com>
2020-07-22 21:01:20 -0700

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar.js';
 import Intro from './components/Intro.js';
