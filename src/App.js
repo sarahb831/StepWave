bc2645bb6189826719c31b804c01cbecc70ba9ad
sarahb831 <smbeauchamp885@gmail.com>
2020-07-22 19:59:59 -0700

    add nav and intro sections

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar.js';
+import Intro from './components/Intro.js';
+import TestComponent from './components/TestComponent.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <NavBar />
+      {/*<header className="App-header">
+        <img src={logo} className="App-logo" alt="StepWave logo" />
+        <TestComponent />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to StepWave.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Come on in
         </a>
       </header>
+  */}
+      <Intro />
     </div>
   );
 }
